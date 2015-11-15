60a28ab73c8809a8c0dfa455f74e5055507b7875
Johnny Mejias <jmejias17@gmail.com>
2015-11-15 18:31:49 -0400

    autogenerate issue

--- blueprints/ember-simple-token/index.js
+++ blueprints/ember-simple-token/index.js
@@ -1,11 +1,5 @@
 module.exports = {
-  description: 'ember-simple-token',
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+  normalizeEntityName() {},
 
   afterInstall() {
     return this.addPackagesToProject([
