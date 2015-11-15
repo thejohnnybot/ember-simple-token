ba290974ef78bfa8781692e5d4e9229540bc789a
Johnny Mejias <jmejias17@gmail.com>
2015-11-15 18:02:18 -0400

    clean blueprint

--- blueprints/ember-cli-simple-token/index.js
+++ blueprints/ember-cli-simple-token/index.js
@@ -1,14 +1,9 @@
 module.exports = {
   description: 'ember-simple-token',
-
   // locals: function(options) {
   //   // Return custom template variables here.
   //   return {
   //     foo: options.entity.options.foo
   //   };
   // }
-
-  afterInstall() {
-    return this.addBowerPackageToProject('ember-simple-auth', '1.0.0');
-  }
 };
