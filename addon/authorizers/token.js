547341d6d2b5d689e6012b5d3296d0d59a79e194
Devin Torres <devin@devintorr.es>
2015-12-09 01:52:30 -0600

    The authorizor should use tokenAttributeName

--- addon/authorizers/token.js
+++ addon/authorizers/token.js
@@ -1,7 +1,16 @@
+import Ember from 'ember';
+import config from 'ember-get-config';
 import BaseAuthorizer from 'ember-simple-auth/authorizers/base';
 
+const { get, isEmpty } = Ember;
+
 export default BaseAuthorizer.extend({
-  authorize({ token }, block) {
-    block('Authorization', `Bearer ${token}`);
+  tokenAttributeName: config['ember-simple-token'].tokenAttributeName || 'token',
+
+  authorize(data, block) {
+    const token = get(data, this.tokenAttributeName);
+    if (!isEmpty(token)) {
+      block('Authorization', `Bearer ${token}`);
+    }
   }
 });
