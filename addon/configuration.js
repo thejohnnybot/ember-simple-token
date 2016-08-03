4d867b6fc2782eb9d858be8ea7c754b323780172
Piotr Pałek <piotrpalek@gmail.com>
2016-08-03 13:53:10 +0200

    fix typeof check

--- addon/configuration.js
+++ addon/configuration.js
@@ -17,7 +17,7 @@ export default {
 
   load(config) {
     for (let property in this) {
-      if (this.hasOwnProperty(property) && Ember.typeOf(property) !== 'function') {
+      if (this.hasOwnProperty(property) && Ember.typeOf(this[property]) !== 'function') {
         this[property] = getWithDefault(config, property, DEFAULTS[property]);
       }
     }
